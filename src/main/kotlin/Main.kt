29cdf49b6da0a079917c9276c215c62608e76621
Ruben Frias <rubenfg2003@gmail.com>
2024-08-19 16:52:18 -0400

    Eliminada clase PruebaMain y Login integrado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,45 @@
+import Views.LoginScreen
+import Views.MainScreen
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import Views.RefugioApp
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import java.awt.Dimension
+import java.awt.GraphicsDevice
+import java.awt.GraphicsEnvironment
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Refugio Animal") {
+@Composable
+@Preview
+fun App(window: java.awt.Window) {
+    var isLoggedIn by remember { mutableStateOf(false) }
+
+    if (isLoggedIn) {
+        // Cambia la ventana a pantalla completa
+        LaunchedEffect(Unit) {
+            setFullScreen(window)
+        }
         RefugioApp()
+    } else {
+        LoginScreen(onLoginSuccess = { isLoggedIn = true })
     }
 }
+
+fun main() = application {
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Login",
+        state = androidx.compose.ui.window.WindowState(width = 800.dp, height = 600.dp)
+    ) {
+        App(window)
+    }
+}
+fun setFullScreen(window: java.awt.Window) {
+    val graphicsDevice: GraphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+    graphicsDevice.fullScreenWindow = window // Configurar la ventana a pantalla completa
+}
+
--- src/main/kotlin/Views/MainContent.kt
+++ src/main/kotlin/Views/MainContent.kt
@@ -3,6 +3,7 @@ package Views
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,6 +23,6 @@ fun MainArea(colors: RefugioColorPalette) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        androidx.compose.material.Text("Contenido principal aquí", color = colors.onBackground)
+        Text("Contenido principal aquí", color = colors.onBackground)
     }
 }
