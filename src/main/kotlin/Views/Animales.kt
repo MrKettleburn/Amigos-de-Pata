7d485bba0995d77f64c3d6e9c188f427b4f95d18
Fabian1820 <149929094+Fabian1820@users.noreply.github.com>
2024-08-21 15:44:13 -0400

    fondo feo okk

--- src/main/kotlin/Views/Animales.kt
+++ src/main/kotlin/Views/Animales.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.Badge
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,15 +22,19 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.draw.clip
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.*
 
 @Composable
 fun AnimalesMostrar(colors: RefugioColorPalette, selectedItem: String, selectedSubItem: String) {
@@ -66,8 +71,7 @@ fun AnimalesMostrar(colors: RefugioColorPalette, selectedItem: String, selectedS
                 modifier = Modifier.padding(bottom = 4.dp)
             )
 
-
-            // Componentes de filtrado
+// Componentes de filtrado
             FilterComponents(
                 colors,
                 onFilterApplied = {
@@ -131,7 +135,6 @@ fun AnimalesMostrar(colors: RefugioColorPalette, selectedItem: String, selectedS
         }
     }
 }
-
 
 @Composable
 fun FilterComponents(
@@ -301,26 +304,10 @@ fun AnimalsExpandableTable(colors: RefugioColorPalette, data: List<AnimalTableRo
 
 @Composable
 fun ActividadesExpandableTable(colors: RefugioColorPalette, data: List<ActividadTableRow>) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(0.dp, 100.dp) // Establecer una altura máxima de 400 dp
-            .clip(RoundedCornerShape(8.dp))
-            .border(
-                width = 1.dp,
-                color = colors.primary,
-                shape = RoundedCornerShape(8.dp)
-            )
-    ) {
-        Column(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .fillMaxSize()
-        ) {
-            data.forEach { row ->
-                ActividadesExpandableRow(colors, row)
-                Divider(color = colors.primary, thickness = 1.5.dp)
-            }
+    LazyColumn {
+        items(data) { row ->
+            ActividadesExpandableRow(colors, row)
+            Divider(color = colors.primary, thickness = 1.5.dp)
         }
     }
 }
@@ -426,8 +413,6 @@ fun ActividadesExpandableRow(colors: RefugioColorPalette, row: ActividadTableRow
             .fillMaxWidth()
             .background(backgroundColor) // Color de fondo para la fila expandida
             .padding(vertical = 8.dp)
-            .heightIn(100.dp) // Establecer una altura máxima para el Column
-
     ) {
         Row(
             modifier = Modifier
@@ -471,10 +456,6 @@ fun ActividadesExpandableRow(colors: RefugioColorPalette, row: ActividadTableRow
         }
 
         if (expanded) {
-            Column(
-                modifier = Modifier
-                    .heightIn(200.dp) // Establecer una altura máxima para el Column anidado
-            ) {
             row.expandedAttributes.forEach { (key, value) ->
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
@@ -492,7 +473,6 @@ fun ActividadesExpandableRow(colors: RefugioColorPalette, row: ActividadTableRow
                     )
                     Text(text = value)
                 }
-            }
             }
         }
 
@@ -531,7 +511,6 @@ data class ActividadTableRow(
     val mainAttributes: Map<String, String>,
     val expandedAttributes: Map<String, String>
 )
-
 
 fun getAnimalsTableRows(animales: List<Animal>): List<AnimalTableRow> {
     val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
@@ -725,7 +704,7 @@ fun ActividadesDialog(
                     }
                 }
 
-                // Componentes de filtrado
+// Componentes de filtrado
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
@@ -782,25 +761,7 @@ fun ActividadesDialog(
                 Spacer(modifier = Modifier.height(16.dp))
 
                 // Tabla de actividades
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.8f)
-                        .clip(RoundedCornerShape(8.dp))
-                        .border(
-                            width = 1.dp,
-                            color = colors.primary,
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .verticalScroll(rememberScrollState())
-                            .heightIn(100.dp) // Establecer la altura máxima deseada
-                    ) {
-                        ActividadesExpandableTable(colors, getActividadesTableRows(actividades))
-                    }
-                }
+                ActividadesExpandableTable(colors, getActividadesTableRows(actividades))
             }
         }
     }
