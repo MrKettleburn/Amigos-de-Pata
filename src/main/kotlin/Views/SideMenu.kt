104c747209cca60923e6543a3ccfae6ff1d7de4c
Fabian1820 <149929094+Fabian1820@users.noreply.github.com>
2024-09-16 23:37:26 -0400

    hash ok

--- src/main/kotlin/Views/SideMenu.kt
+++ src/main/kotlin/Views/SideMenu.kt
@@ -118,7 +118,6 @@ fun AnimatedSideMenu(
             Text("Donar")
         }
 
-        // Botón de Cerrar Sesión
         Button(
             onClick = onLogout,
             modifier = Modifier
